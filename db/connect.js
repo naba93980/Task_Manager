4ee3d6f8de75ba74e99fe30d3eab646da0893202
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-11 21:44:36 +0530

    function to connect to the database

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+const connectDB = (url) => {
+  return mongoose.connect(url);
+};
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("connected"))
-  .catch((err) => console.log("connection failed -  " + err));
-
-mongoose.disconnect();
+module.exports.connectDB = connectDB;
