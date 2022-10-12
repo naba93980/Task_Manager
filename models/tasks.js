0224175de0cbfc884df70abcb5fa87a0b9618b5c
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-12 21:24:25 +0530

    added validations in schema definition

--- models/tasks.js
+++ models/tasks.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 const TaskSchema = new mongoose.Schema({
-  name: { type: String },
-  completed: { type: Boolean },
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "name must be added"],
+    maxLength: [20, "name cannot be more than 20 words"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports.TaskCollection = mongoose.model("TaskCollection", TaskSchema);
