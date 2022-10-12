129128a920e2e321e290960b2a5d1ce8bfae5d02
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-12 21:21:59 +0530

    extracted tasks data using object destructuring

--- public/browser-app.js
+++ public/browser-app.js
@@ -7,9 +7,7 @@ const formAlertDOM = document.querySelector('.form-alert')
 const showTasks = async () => {
   loadingDOM.style.visibility = 'visible'
   try {
-    const {
-      data: { tasks },
-    } = await axios.get('/api/v1/tasks')
+    const { data: tasks } = await axios.get("/api/v1/tasks");
     if (tasks.length < 1) {
       tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>'
       loadingDOM.style.visibility = 'hidden'
