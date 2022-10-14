9e65e4dd6e91f26a2e6c923a13044d904c423e50
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-14 22:12:24 +0530

    custom class for creating error objects

---
+++ error/custom-error.js
@@ -0,0 +1,10 @@
+class CustomError extends Error{
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+const createCustomError = (message, statusCode) => {
+    return new CustomError(message, statusCode);
+}
+module.exports = { createCustomError, CustomError };
