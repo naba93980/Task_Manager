d71fc79feb9bf455667a18adaf8d5f50f8d7f6ab
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-14 22:03:33 +0530

    added errorHandler, notFound middleware functions and PORT in environment

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const { tasks } = require('./routes/tasks');
 const { connectDB } = require('./db/connect');
+const notFound = require('./middleware/notFound');
+const errorHandler = require('./middleware/handleError')
 require("dotenv").config();
 
 app.use(express.static('./public'));
 
-//routes
-app.get('/', (req, res) => {
-  res.status(200);
-})
-
 app.use(express.json());
+
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const start = async () => {
   try {
